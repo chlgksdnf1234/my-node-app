8720c2ee7bbe9f6ce3d3fee62d0948f8f218ddd9
chlgksdnf1234 <chlgksdnf1234@gmail.com>
2026-08-19 18:50:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Node.js 무료 웹 서버가 정상 작동 중입니다!</h1>');
+  res.send(`
+    <div style="text-align: center; margin-top: 50px; font-family: sans-serif;">
+      <h1>🚀 나만의 웹사이트 수정 성공!</h1>
+      <p>Node.js와 Docker를 이용해 만든 웹페이지입니다.</p>
+    </div>
+  `);
 });
 
 app.listen(3000, () => {
